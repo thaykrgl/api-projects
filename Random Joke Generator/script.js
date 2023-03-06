3f82b4ff8ec2672a8e77f0e6b3e8d684e2f407aa
Taha AYKIROĞLU <taha.aykiroglu@karaca.com>
2023-03-06 22:41:03 +0300

    Added Functionality for fade Class

--- Random Joke Generator/script.js
+++ Random Joke Generator/script.js
@@ -3,10 +3,13 @@ const btn = document.getElementById("btn");
 const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
 
 let getJoke = () => {
+    jokeContainer.classList.remove("fade");
     fetch(url)
         .then(data => data.json())
         .then(item => {
             jokeContainer.textContent = `${item.joke}`;
+            jokeContainer.classList.add("fade")
+
         });
 }
 
